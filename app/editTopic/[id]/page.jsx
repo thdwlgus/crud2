316e8dfadecba54e588a97bf9c3b0ea072b11426
thdwlgus <jiniluce7@naver.com>
2023-11-27 17:51:20 +0900

    1st

---
+++ app/editTopic/[id]/page.jsx
@@ -0,0 +1,30 @@
+import EditTopicForm from '@/components/EditTopicForm'
+import React from 'react'
+
+const getTopicById = async (id) => {
+  const apiUrl = process.env.API_URL
+  try {
+    const res = await fetch(`${apiUrl}http://localhost:3000/api/topics/${id}`, {
+      method: 'GET',
+      cache: 'no-store',
+    })
+    if (!res.ok) {
+      throw new Error('Failed to fetch topic')
+    }
+    return res.json()
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export default async function EditTopicPage({ params }) {
+  const { id } = params
+  const { topic } = await getTopicById(id)
+  const { title, description } = topic
+
+  return (
+    <div>
+      <EditTopicForm id={id} title={title} description={description} />
+    </div>
+  )
+}
